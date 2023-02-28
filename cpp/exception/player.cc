d390ef636a574377f14a16184b70e985a9d5adfe
Noctis <carl10vano@gmail.com>
2023-02-28 22:14:23 +0100

    exception

--- cpp/exception/player.cc
+++ cpp/exception/player.cc
@@ -6,7 +6,7 @@
 Player::Player(const std::string& name, unsigned int age)
 {
     if (name.empty())
-        throw InvalidArgumentException("Name can't be empty");
+        throw InvalidArgumentException("Name can't be empty.");
 
     if (age > 150)
         throw InvalidArgumentException("Sorry gramp, too old to play.");
