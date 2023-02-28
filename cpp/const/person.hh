ea6b41b482c9fb9b182b88c51b88a66a5766947e
Noctis <carl10vano@gmail.com>
2023-02-28 20:50:05 +0100

    add: const file

---
+++ cpp/const/person.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+
+class Person
+{
+    public:
+        Person(const std::string& name, unsigned int age);
+        std::string name_get() const;
+        unsigned int age_get() const;
+        void name_set(const std::string name);
+        void age_set(unsigned int);
+
+    private:
+        std::string name_;
+        unsigned int age_;
+};
